70d398aad4f7b36fa6bd40b10166b13b43654fc7
Ashish <ashishkumarsvrp1@gmail.com>
2025-06-15 01:32:54 +0530

    ✅ Phase 6: Added profile photo upload, profile edit, and new users app

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users.apps.UsersConfig',
     'notes',
     'accounts',
 ]
@@ -138,3 +139,7 @@ import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
